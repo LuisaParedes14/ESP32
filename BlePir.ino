53e251263756b571ef3faca4a64475432625a752
LuisaParedes14 <83262993+LuisaParedes14@users.noreply.github.com>
2021-09-22 12:36:14 -0500

    Update BlePir.ino

--- BlePir.ino
+++ BlePir.ino
@@ -18,56 +18,56 @@ BLECharacteristic controlCharacteristic(
 CONTROL_CHARACTERISTIC_UUID,BLECharacteristic::PROPERTY_READ |BLECharacteristic::PROPERTY_WRITE);
 BLECharacteristic *nameCharacteristic;
 void setup() {
-Serial.begin(115200);
-Serial.println("Starting BLE!");
-Serial.println("Initializing device");
-BLEDevice::init("Detectar Presencia"); // Initializing the device with its name
-Serial.println("Creating server");
-BLEServer *pServer = BLEDevice::createServer(); // Create the server
-Serial.println("Adding service UUID");
-BLEService *pService = pServer->createService(SERVICE_UUID); // Creating a new service into server
-// Adding a characteristic with the object name (official UUID), without object (this characteristic will not change)
-Serial.println("Adding name characteristic");
-nameCharacteristic = pService->createCharacteristic(
-BLEUUID((uint16_t)0x2A00),
-BLECharacteristic::PROPERTY_READ);
-nameCharacteristic->setValue(MessagePir.c_str());
-// Adding a characteristic to control the led with 0 and 1
-Serial.println("Adding control characteristic");
-pService->addCharacteristic(&controlCharacteristic);
-controlCharacteristic.setValue(&ledStatus); // Value uint8_t with length 1
-Serial.println("Starting...");
-pService->start();
-Serial.println("Creating advertising");
-// BLEAdvertising *pAdvertising = pServer->getAdvertising(); // this still is working for backward compatibility
-BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-pAdvertising->addServiceUUID(SERVICE_UUID);
-pAdvertising->setScanResponse(true);
-pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
-pAdvertising->setMinPreferred(0x12);
-BLEDevice::startAdvertising();
-Serial.println("Characteristic defined! Now you can read it in your phone!");
-pinMode (LED, OUTPUT); // Set the LED pin as OUTPUT
-pinMode(ledPin, OUTPUT); //El pin 12 del arduino lo asignamos como salida para el led
-pinMode(sensorPin, INPUT);//El pin 11 lo asignamos como entrada para la señal del sensor
-for(int i = 0; i > 30; i++) //Utilizamos un for para calibrar el sensor depende del tipo de sensor que utilicemos va a cambiar el tiempo de calibración
-{
-delay(1000);
-}
-delay(50);
+  Serial.begin(115200);
+  Serial.println("Starting BLE!");
+  Serial.println("Initializing device");
+  BLEDevice::init("Detectar Presencia"); // Initializing the device with its name
+  Serial.println("Creating server");
+  BLEServer *pServer = BLEDevice::createServer(); // Create the server
+  Serial.println("Adding service UUID");
+  BLEService *pService = pServer->createService(SERVICE_UUID); // Creating a new service into server
+  // Adding a characteristic with the object name (official UUID), without object (this characteristic will not change)
+  Serial.println("Adding name characteristic");
+  nameCharacteristic = pService->createCharacteristic(
+  BLEUUID((uint16_t)0x2A00),
+  BLECharacteristic::PROPERTY_READ);
+  nameCharacteristic->setValue(MessagePir.c_str());
+  // Adding a characteristic to control the led with 0 and 1
+  Serial.println("Adding control characteristic");
+  pService->addCharacteristic(&controlCharacteristic);
+  controlCharacteristic.setValue(&ledStatus); // Value uint8_t with length 1
+  Serial.println("Starting...");
+  pService->start();
+  Serial.println("Creating advertising");
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising(); // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+  pinMode (LED, OUTPUT); // Set the LED pin as OUTPUT
+  pinMode(ledPin, OUTPUT); //El pin 12 del arduino lo asignamos como salida para el led
+  pinMode(sensorPin, INPUT);//El pin 11 lo asignamos como entrada para la señal del sensor
+  for(int i = 0; i > 30; i++) //Utilizamos un for para calibrar el sensor depende del tipo de sensor que utilicemos va a cambiar el tiempo de calibración
+  {
+  delay(1000);
+  }
+  delay(50);
 }
 void loop() {
-std::string controlValue = controlCharacteristic.getValue();
-if (controlValue[0] != ledStatus) {
-Serial.print("Value changed... new value: ");
-Serial.println(controlValue[0]);
-ledStatus = controlValue[0];
-if (ledStatus == 48) {
-digitalWrite(LED, LOW); // LED Off
-}
-else if (ledStatus == 49) {
-digitalWrite(LED, HIGH); // LED On
-}
+  std::string controlValue = controlCharacteristic.getValue();
+  if (controlValue[0] != ledStatus) {
+  Serial.print("Value changed... new value: ");
+  Serial.println(controlValue[0]);
+  ledStatus = controlValue[0];
+  if (ledStatus == 48) {
+  digitalWrite(LED, LOW); // LED Off
+  }
+  else if (ledStatus == 49) {
+  digitalWrite(LED, HIGH); // LED On
+  }
 }
 val = digitalRead(sensorPin); //Lee el valor de la variable (val)
 if (val == HIGH) //Si detecta que hay movimiento manda activar el led que hay conectado en el pin 12 del arduino
